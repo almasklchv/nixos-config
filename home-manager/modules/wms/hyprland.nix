75372d534d7eb842a29f6d36abb27b0859e10d89
Almas Kilichov <a.kilichov@documentolog.com>
2024-08-28 10:31:17 +0500

    add env variables for nvidia support

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -15,6 +15,12 @@
         "XCURSOR_SIZE,36"
         "QT_QPA_PLATFORM,wayland"
         "XDG_SCREENSHOTS_DIR,~/screens"
+        "LIBVA_DRIVER_NAME,nvidia"
+        "XDG_SESSION_TYPE,wayland"
+        "GBM_BACKEND,nvidia-drm"
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        "WLR_NO_HARDWARE_CURSORS,1"
+        "NIXOS_OZONE_WL,1"
       ];
 
       debug = {
